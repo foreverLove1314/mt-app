7ee38e2edd890020256c7ee66e87be2a3cb69a53
tx <302871586@qq.com>
2019-07-30 22:08:17 +0800

    完成部分头部布局

---
+++ components/index/menu.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="m-menu">
+    <dl class="nav">
+      <dt>全部分类</dt>
+      <dd v-for="(item,index) in menu" :key="index">
+        <i :class="item.type"></i>{{item.name}}<span class="arrow"></span>
+      </dd>
+    </dl>
+  </div>
+</template>
+
+<script>
+export default {
+	name: "emenu",
+  data(){
+	  return {
+	    menu:[
+        {
+          type:'food',
+          name:'美食'
+        },
+        {
+          type:'takeout',
+          name:'外卖'
+        },
+        {
+          type:'hotel',
+          name:'酒店'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
-  <el-container>
-    <el-header>
+  <el-container class="layout-default">
+    <el-header height="197px">
       <my-header/>
     </el-header>
     <el-main>
       <nuxt />
     </el-main>
-    <el-footer>
+    <el-footer height="100%">
       <my-footer/>
     </el-footer>
   </el-container>
