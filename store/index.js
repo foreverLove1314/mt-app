fb15b08dbffc17a9a8a901395541f2974a16cad8
tx <302871586@qq.com>
2019-08-18 12:33:45 +0800

    优化部分代码

--- store/index.js
+++ store/index.js
@@ -1,26 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import geo from './modules/geo'
+import home from './modules/home'
 
 Vue.use(Vuex)
 
-const store = () => new Vuex.Store({
-    modules: {
-        geo
-    },
-    actions: {
-        async nuxtServerInit({commit}, {req, app}) {
-            const {status, data: {province, city}} = await app.$axios.get('/geo/getPosition')
-            commit('geo/setPosition',status===200?{city,province}:{city:'',province:''})
-        }
-    }
-})
-// const actions={
-//     async nuxtServerInit({commit}, {req, app}) {
-//     const {status, data: {province, city}} = await app.$axios.get('/geo/getPosition')
-//     commit('geo/setPosition',status===200?{city,province}:{city:'',province:''})
-// }
-// }
-// export {geo,actions}
+const modules = { geo, home }
 
-export default store
+const actions = {
+  async nuxtServerInit({ commit }, { req, app }) {
+    const { status, data: { province, city } } = await app.$axios.get('/geo/getPosition')
+    commit('geo/setPosition', status === 200 ? { province, city } : { province: '', city: '' })
+    // const { status: status2, data: { menu } } = await app.$axios.get('/geo/menu')
+    // commit('home/setMenu', status2 === 200 ? menu : [])
+  }
+}
+
+export default {
+  modules,
+  actions
+}
