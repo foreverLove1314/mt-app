69d0d037fddaa1eb61353e4b115ade74710af3ae
tx <302871586@qq.com>
2019-10-27 18:10:54 +0800

    完成产品详情页接口和页面开发，完成购物车部分页面开发

---
+++ pages/cart.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="page-cart">
+    <el-row>
+      <el-col v-if="1 || cart.length" :span="24" class="m-cart">
+        <list/>
+        <p>应付金额：<em class="money">￥{{total}}</em></p>
+        <div class="post">
+          <el-button type="primary" @click="submitOrder">提交订单</el-button>
+        </div>
+      </el-col>
+      <el-col v-else :span="24" class="empty">购物车为空</el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import List from '@/components/cart/list.vue'
+export default {
+  name: "cart",
+  components:{
+    List
+  },
+  data() {
+    return {
+      cart:[]
+    }
+  },
+  computed:{
+    total(){
+      let total = 0;
+      this.cart.forEach(item=>{
+        total+=item.price*item.count;
+      });
+      return total;
+    }
+  },
+  methods:{
+    submitOrder(){
+    
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "@/assets/css/cart/index.scss";
+</style>
