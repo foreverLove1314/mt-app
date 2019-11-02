6ce3562f1fdeb37831585ea7c7c63fa3f2576866
tx <302871586@qq.com>
2019-11-02 16:56:37 +0800

    完成订单接口，项目开发完成

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,8 +1,8 @@
 <template>
 	<div class="page-cart">
     <el-row>
-      <el-col v-if="1 || cart.length" :span="24" class="m-cart">
-        <list/>
+      <el-col v-if="cart.length" :span="24" class="m-cart">
+        <list :cart-data="cart"/>
         <p>应付金额：<em class="money">￥{{total}}</em></p>
         <div class="post">
           <el-button type="primary" @click="submitOrder">提交订单</el-button>
@@ -35,8 +35,36 @@ export default {
     }
   },
   methods:{
-    submitOrder(){
-    
+    async submitOrder(){
+      let {status,data:{code,id}} = await this.$axios.post('/order/createOrder',{
+        count: this.cart[0].count,
+        price: this.cart[0].price,
+        id:this.cartNo
+      })
+      if(status === 200 && code === 0){
+        this.$alert(`恭喜您，已成功下单,订单号：${id}`,'下单成功',{
+          confirmButtonText:'确定',
+          callback:action=>{
+            window.location.href='/order'
+          }
+        })
+      }
+    }
+  },
+  async asyncData(ctx){
+    let {status,data:{code,data:{name,price}}} = await ctx.$axios.post('/cart/getCart',{
+      id: ctx.query.id
+    })
+    console.log(status);
+    if (status === 200){
+      return {
+        cart:[{
+          name,
+          price,
+          count:1
+        }],
+        cartNo:ctx.query.id
+      }
     }
   }
 }
